f64203c5ae31c7fc593934a6fef5029c42e6cc93
Flutter Developer <56519330+mdrahamat0505@users.noreply.github.com>
2023-10-08 15:32:05 +0600

    update.

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:ecomerce_app/Pages/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    ),
+  );
+}
